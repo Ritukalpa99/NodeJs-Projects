e4958cfe57ee72ef057856d4a1780feae02a2d67
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-10-03 13:04:13 +0530

    routing requests

--- Kickstarting NodeJS/02-app.js
+++ Kickstarting NodeJS/02-app.js
@@ -7,12 +7,20 @@ const http = require('http');
 // http.createServer(rqListener)
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
     res.write('<body><h1>Hello from Node.js server!</h1></body>')
     res.write('</html>')
+    res.end()
 })
 
 
