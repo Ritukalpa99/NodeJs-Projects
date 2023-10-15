ce8d493aff2fc8391ac1ed273d49ebae1541e864
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-10-15 15:10:29 +0530

    exports added

---
+++ Kickstarting NodeJS/routes.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const requestHandler = (req,res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url === '/message') {
+        const body = []
+        req.on('data', (chunks) => {
+            body.push(chunks)
+        })
+        req.on('end',() => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        })
+        
+        // res.writeHead(302, {})
+        res.statusCode = 302 //redirection
+        res.setHeader('Location', '/')
+        
+        return res.end()
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Hello from Node.js server!</h1></body>')
+    res.write('</html>')
+    res.end()
+}
+
+module.exports = requestHandler;
