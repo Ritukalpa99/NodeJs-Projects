0fec8da5c8159fbea256f2dcb476cfbaef334036
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-10-19 03:06:15 +0530

    chat form added, redirecting success

--- Chat App/app.js
+++ Chat App/app.js
@@ -15,13 +15,26 @@ app.post('/user-details', (req,res) => {
     const userName = req.body.name
     console.log(userName);
     fs.appendFile('./message.txt', `${userName}\n`, (err) => {
-        console.log(err);
+        // console.log(err);
     })
     // localStorage.setItem({'user' : userName})
     res.redirect('/')
 })
 
+app.post('/chat-message', (req,res) => {
+    const chat = req.body.message
+    fs.appendFile('./message.txt', `${chat}\n`, (err) => {
+        // console.log(err);
+    })
+    res.redirect('/')
+})
+
 app.get('/', (req,res) => {
-    res.send('<h1>Welcome</h1>')
+    fs.readFile('./message.txt',"utf-8", (err,data) => {
+        if(err) throw err;
+        const text = data + '<form action="/chat-message" method="POST"><input type="text" name="message"><button type="submit">send</button></form>'
+        // res.send(data)
+        res.send(text)
+    })
 })
 app.listen(3000);
