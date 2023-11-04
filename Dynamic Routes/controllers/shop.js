1a857f3a9cff776e67d08a64e224e23f55c4a61d
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-11-04 19:29:15 +0530

    product delete from cart

--- Dynamic Routes/controllers/shop.js
+++ Dynamic Routes/controllers/shop.js
@@ -51,7 +51,7 @@ exports.getCart = (req, res, next) => {
 			res.render("shop/cart", {
 				path: "/cart",
 				pageTitle: "Your Cart",
-				products : cartProducts
+				products: cartProducts,
 			});
 		});
 	});
@@ -63,6 +63,14 @@ exports.postCart = (req, res, next) => {
 		Cart.addProduct(prodId, product.price);
 	});
 	res.redirect("/cart");
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.deleteProduct(prodId, product.price);
+		res.redirect("/cart");
+	});
 };
 
 exports.getOrders = (req, res, next) => {
