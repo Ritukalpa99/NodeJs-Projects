8b4dd0adf9f9c2f1a23ca9f8a248b65a73a34523
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-11-04 10:55:33 +0530

    added query params

--- Dynamic Routes/models/cart.js
+++ Dynamic Routes/models/cart.js
@@ -28,7 +28,6 @@ module.exports = class Cart {
 				updatedProduct = { ...existingProduct };
 				updatedProduct.qty = updatedProduct.qty + 1;
 				cart.products = [...cart.products];
-				cart.products[existingProductIndex] = updatedProduct;
 			} else {
 				updatedProduct = { id: id, qty: 1 };
 				cart.products = [...cart.products, updatedProduct];
