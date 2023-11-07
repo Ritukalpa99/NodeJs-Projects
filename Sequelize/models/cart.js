44a64b2556ac4bb342d7db23cb20dff9ccf11b74
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-11-07 16:57:53 +0530

    sequelize setup done

---
+++ Sequelize/models/cart.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+	path.dirname(process.mainModule.filename),
+	"data",
+	"cart.json"
+);
+
+module.exports = class Cart {
+	static addProduct(id, productPrice) {
+		// Fetch the previous cart
+		fs.readFile(p, (err, fileContent) => {
+			let cart = { products: [], totalPrice: 0 };
+			if (!err) {
+				cart = JSON.parse(fileContent);
+			}
+			// Analyse the cart => find existing product
+			const existingProductIndex = cart.products.findIndex(
+				(p) => p.id === id
+			);
+			const existingProduct = cart.products[existingProductIndex];
+
+			// Add new product/increase the quantity
+			let updatedProduct;
+			if (existingProduct) {
+				console.log("here");
+				updatedProduct = { ...existingProduct };
+				updatedProduct.qty = updatedProduct.qty + 1;
+				cart.products = [...cart.products];
+			} else {
+				updatedProduct = { id: id, qty: 1 };
+				cart.products = [...cart.products, updatedProduct];
+			}
+			cart.totalPrice = cart.totalPrice + +productPrice;
+			fs.writeFile(p, JSON.stringify(cart), (err) => {
+				console.log(err);
+			});
+		});
+	}
+
+	static deleteProduct(id, productPrice) {
+		fs.readFile(p, (err, fileContent) => {
+			if (err) {
+				return;
+			}
+			const updatedCart = { ...JSON.parse(fileContent) };
+			const product = updatedCart.products.findIndex(
+				(prod) => prod.id === id
+			);
+			if(!product) {
+				return;
+			}
+			const productQty = product.qty;
+			updatedCart.products = updatedCart.products.filter(
+				(prod) => prod.id !== id
+			);
+			updatedCart.totalPrice =
+				updatedCart.totalPrice - productQty * productPrice;
+
+			fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+				console.log(err);
+			});
+		});
+	}
+
+	static getCart(cb) {
+		fs.readFile(p, (err,fileContent) => {
+			const cart = JSON.parse(fileContent)
+			if(err) {
+				cb(null);
+			} else {
+				cb(cart)
+			}
+		})
+	}
+};
