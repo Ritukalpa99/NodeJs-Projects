a5e585da66e059f1a96d4e1c99eea40a786b9a2b
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2023-12-16 16:10:57 +0530

    server side basic functionality done

---
+++ Test Project/server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const playerRoutes = require("./routes/player");
+
+const sequelize = require("./util/database");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/players", playerRoutes);
+
+sequelize
+	.sync()
+	// .sync({force : true})
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running at ${PORT}`);
+		});
+	});
+
