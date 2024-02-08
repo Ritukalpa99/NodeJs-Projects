8fc622a1fccc0f572d9e37daa0b1eb7edf98830d
Ritukalpa99 <ritukalpa.gogoi99@gmail.com>
2024-02-08 17:38:36 +0530

    db connection established

--- Using MongoDB/util/database.js
+++ Using MongoDB/util/database.js
@@ -1,8 +1,28 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+require("dotenv").config();
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(process.env.MONGO_URL).then(
+    (client)  =>  {
+      console.log('Connected');
+      _db = client.db('shop');
+      callback()
+    }
+  ).catch(err => {
+    console.log(err);
+    throw err;
+  });
+}
+
+const getDB = ()=> {
+  if(_db) {
+    return _db
+  }
+  throw 'No DB found'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB
